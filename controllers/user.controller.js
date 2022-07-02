7f4b27929fae8c1bc13e196ac4f74e074d60d762
Da-zeros <daniromanpt@gmail.com>
2022-07-02 20:17:20 +0200

    Auth done

---
+++ controllers/user.controller.js
@@ -0,0 +1,100 @@
+const User = require("../models/User.model")
+const validator = require('validator');
+
+function signup(req, res){
+    res.render("auth/signup");
+}
+
+async function signupPost(req, res){
+
+    const { username, password } = req.body;
+    
+    if(validator.isEmpty(username) || validator.isEmpty(password)){
+        return res
+            .status(500)
+            .render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username and password.'});
+        }
+    if (!validator.isStrongPassword(password)) {
+        return res
+          .status(500)
+          .render('auth/signup', { errorMessage: 'Password needs to have at least 8 chars and must contain at least one number, one lowercase and one uppercase letter and especial caracter.' });
+      }
+
+    try {
+        const user = await User.createUser( username, password)
+        if (user) return res.render("auth/login")
+        else{
+            return res.status(400).render("auth/signup", {
+                errorMessage:
+                "Username need to be unique. The username you chose is already in use.",
+            });
+        }
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return res
+                .status(400)
+                .render("auth/signup", { errorMessage: error.message });
+            }
+            
+            return res
+            .status(500)
+            .render("auth/signup", { errorMessage: error.message });
+    }
+      
+}
+
+function login(req, res){
+    res.render("auth/login");
+}
+
+async function loginPost(req, res, next){
+    
+    const { username, password } = req.body;
+
+    if(validator.isEmpty(username) || validator.isEmpty(password)){
+        return res
+            .status(500)
+            .render('auth/login', { errorMessage: 'All fields are mandatory. Please provide your username and password.'});
+    }
+
+    try {
+        
+        const userLoged = await User.loginUser(username, password)
+ 
+      // If the user isn't found, send the message that user provided wrong credentials
+      if (!userLoged) {
+            res.status(400).render("auth/login", {
+            errorMessage: "Wrong credentials.",
+        });
+      }else{
+        req.session.user = userLoged;
+        // req.session.user = user._id; // ! better and safer but in this case we saving the entire user object
+        return res.redirect("/");
+      }
+
+    } catch (err) {
+         // in this case we are sending the error handling to the error handling middleware that is defined in the error handling file
+      // you can just as easily run the res.status that is commented out below
+      next(err);
+      // return res.status(500).render("login", { errorMessage: err.message });
+    }  
+}
+
+function logOut(req, res){
+    req.session.destroy((err) => {
+        if (err) {
+          return res
+            .status(500)
+            .render("auth/logout", { errorMessage: err.message });
+        }
+        res.redirect("/");
+      });
+}
+
+module.exports = {
+    signup,
+    signupPost,
+    login,
+    loginPost,
+    logOut
+}
--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,7 @@
 const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose")
+// ℹ️ Handles password encryption
+const bcrypt = require("bcrypt");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
@@ -14,6 +17,59 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function() {
+  const user = this
+  
+  try {
+    if (this.isModified("password") || this.isNew){
+      const salt = await bcrypt.genSalt(10)
+      
+      const hash = await bcrypt.hash(user.password, salt)
+      
+      user.password = hash
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+userSchema.statics.createUser = async function(username, password){
+  // Search the database for a user with the username submitted in the form
+  
+  const findUser = await this.findOne({ username })
+  try{
+    if(!findUser){
+      const newUser = await new this({
+        username,
+        password
+        });
+        newUser.save()
+        return newUser
+    }
+   
+  }catch(Error){
+     console.log(Error)     
+  }
+} 
+
+userSchema.statics.loginUser = async function(username, password){
+  
+  const findUserLog = await this.findOne({ username })
+  
+  try{
+    if(findUserLog){
+      const isSamePassword = await bcrypt.compare(password, findUserLog.password)
+        if (isSamePassword) {
+          return findUserLog
+        }
+    }
+   
+  }catch(Error){
+     console.log(Error)     
+  }
+}
+
 
 const User = model("User", userSchema);
 
